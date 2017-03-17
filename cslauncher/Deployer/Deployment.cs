b28087e98825ff90c0d64a0b31e3f975d18ef41a
Rodrigue Cloutier <rodcloutier@gmail.com>
2017-03-17 13:09:59 -0400

    Added option to override binPath and installPath Added command to get the binPath Added default binPath and installPath location. Added environment variable expansion in paths

--- cslauncher/Deployer/Deployment.cs
+++ cslauncher/Deployer/Deployment.cs
@@ -1,8 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
+
 using CSLauncher.LauncherLib;
-using System;
 
 namespace CSLauncher.Deployer
 {
@@ -97,7 +98,7 @@ namespace CSLauncher.Deployer
             return path;
         }
 
-        public static Deployment Read(string path)
+        public static Deployment Read(string path, string optionBinPath, string optionInstallPath)
         {
             FileStream fileStream = new FileStream(path, FileMode.Open);
             DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(Deployment));
@@ -107,6 +108,31 @@ namespace CSLauncher.Deployer
             dep.FileDateTime = File.GetLastWriteTimeUtc(path);
             dep.LauncherPath = RootPath(dep.LauncherPath);
             dep.LauncherLibPath = RootPath(dep.LauncherLibPath);
+
+            if (optionBinPath != null )
+            {
+                dep.BinPath = optionBinPath;
+            }
+
+            if ( dep.BinPath == null )
+            {
+                dep.BinPath = Path.Combine("%USERPROFILE%", "bin");
+            }
+
+            if (optionInstallPath != null )
+            {
+                dep.InstallPath = optionInstallPath;
+            }
+    
+            if (dep.InstallPath == null )
+            {
+                dep.InstallPath = Path.Combine(dep.BinPath, "packages");
+            }
+
+            dep.LauncherPath = Environment.ExpandEnvironmentVariables(dep.LauncherPath);
+            dep.LauncherLibPath = Environment.ExpandEnvironmentVariables(dep.LauncherLibPath);
+            dep.BinPath = Environment.ExpandEnvironmentVariables(dep.BinPath);
+            dep.InstallPath = Environment.ExpandEnvironmentVariables(dep.InstallPath);
 
             return dep;
         }
