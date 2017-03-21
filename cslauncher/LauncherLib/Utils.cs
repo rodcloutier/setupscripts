cc8e41cd412b6c2749d9f52be559a52e8e878419
jelmansouri <jalal-eddine.elmansouri@ubisoft.com>
2017-03-20 21:25:54 -0400

    Add forgotten file

---
+++ cslauncher/LauncherLib/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLauncher.LauncherLib
+{
+    public class Utils
+    {
+        public static void AddOrSetEnvVariable(StringDictionary envVariables, string key, string value)
+        {
+            if (envVariables.ContainsKey(key))
+                envVariables[key] = value;
+            else
+                envVariables.Add(key, value);
+        }
+    }
+}
